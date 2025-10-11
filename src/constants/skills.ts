545dc86c349272779eaa188a76bedf36885fa0c4
kristofajosh <christopherjoshua18@live.com>
2025-10-11 09:04:07 +0100

    chore: initial setup

---
+++ src/constants/skills.ts
@@ -0,0 +1,40 @@
+export const Skills = {
+    frontend: [
+        { name: 'React', logo: 'react-original'},
+        { name: 'Svelte', logo: 'svelte-plain'},
+        { name: 'TypeScript', logo: 'typescript-plain'},
+        { name: 'Tailwind CSS', logo: 'tailwindcss-original'},
+        { name: 'Next.js', logo: 'nextjs-plain'},
+        { name: 'Redux', logo: 'redux-original'},
+        { name: 'GraphQL', logo: 'graphql-plain'},
+        { name: 'Jest', logo: 'jest-plain'},
+        { name: 'Vitest', logo: 'vitest-plain'},
+        { name: 'Playwright', logo: 'playwright-plain'},
+        { name: 'Storybook', logo: 'storybook-plainbackend'}
+    ],
+    backend: [
+        { name: 'Node.js', logo: 'nodejs-plain' },
+        { name: 'Python', logo: 'python-plain' },
+        { name: 'Express.js', logo: 'express-original' },
+        { name: 'Fastify', logo: 'fastify-plain' },
+        { name: 'FastApi', logo: 'fastapi-plain' },
+        { name: 'Flask', logo: 'flask-original' },
+        { name: 'GraphQL', logo: 'graphql-plain' },
+        { name: 'PostgreSQL', logo: 'postgresql-plain' },
+        { name: 'MongoDB', logo: 'mongodb-plain' },
+        { name: 'Redis', logo: 'redis-plain' },
+    ],
+    devops: [
+        { name: 'Docker', logo: 'docker-plain' },
+        { name: 'AWS', logo: 'amazonwebservices-plain-wordmark' },
+        { name: 'Azure', logo: 'azure-plain' },
+        { name: 'Google Cloud', logo: 'googlecloud-plain' },
+        { name: 'Jenkins', logo: 'jenkins-plain' },
+        { name: 'GitHub Actions', logo: 'githubactions-plain' },
+    ],
+    certifications:[
+        { name: 'Google Developers Group', logo: 'google-plain' },
+        { name: 'Cisco Certified Network Associate (CCNA)', logo: 'cisco-plain' },
+        { name: 'CompTIA Security+', logo: 'comptia-plain' },
+    ]
+}
---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import devtoolsJson from 'vite-plugin-devtools-json';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		devtoolsJson(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	],
+	test: {
+		expect: { requireAssertions: true },
+		projects: [
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'client',
+					environment: 'browser',
+					browser: {
+						enabled: true,
+						provider: 'playwright',
+						instances: [{ browser: 'chromium' }]
+					},
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts']
+				}
+			},
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
+	}
+});
