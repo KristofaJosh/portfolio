049bb899429d6777ec2a62196a0faef0415dad0d
kristofajosh <christopherjoshua18@live.com>
2025-11-08 21:20:35 +0100

    fix: put ph behind proxy

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
-import { sequence } from '@sveltejs/kit/hooks';
+import { paraglideMiddleware } from '$lib/paraglide/server';
 import * as auth from '$lib/server/auth';
 import type { Handle } from '@sveltejs/kit';
-import { paraglideMiddleware } from '$lib/paraglide/server';
+import { sequence } from '@sveltejs/kit/hooks';
 
 const handleParaglide: Handle = ({ event, resolve }) => paraglideMiddleware(event.request, ({ request, locale }) => {
 	event.request = request;
@@ -33,4 +33,39 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 	return resolve(event);
 };
 
-export const handle: Handle = sequence(handleParaglide, handleAuth);
+const handlePH: Handle = async ({ event, resolve }) => {
+	const { pathname } = event.url;
+
+	const path = '/internal-ph';
+
+	if (pathname.startsWith('/internal-ph')) {
+		// Determine target hostname based on static or dynamic ingestion
+		const hostname = pathname.startsWith(`${path}/static/`)
+			? 'eu-assets.i.posthog.com' // change eu to eu for EU Cloud
+			: 'eu.i.posthog.com'; // change eu to eu for EU Cloud
+
+		// Build external URL
+		const url = new URL(event.request.url);
+		url.protocol = 'https:';
+		url.hostname = hostname;
+		url.port = '443';
+		url.pathname = pathname.replace(`${path}/`, '');
+
+		// Clone and adjust headers
+		const headers = new Headers(event.request.headers);
+		headers.set('Accept-Encoding', '');
+		headers.set('host', hostname);
+
+		// Proxy the request to the external host
+		return await fetch(url.toString(), {
+			method: event.request.method,
+			headers,
+			body: event.request.body,
+			duplex: 'half'
+		});
+	}
+
+	return await resolve(event);
+}
+
+export const handle: Handle = sequence(handleParaglide, handleAuth, handlePH);
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,15 +1,19 @@
 import posthog from 'posthog-js';
 import { browser } from '$app/environment';
-import { PUBLIC_POSTHOG_KEY, PUBLIC_POSTHOG_HOST } from '$env/static/public';
+import { PUBLIC_POSTHOG_KEY } from '$env/static/public';
 
 export const load = async () => {
 	if (browser) {
 		posthog.init(PUBLIC_POSTHOG_KEY, {
-			api_host: PUBLIC_POSTHOG_HOST,
+			api_host: '/internal-ph',
+			ui_host: 'https://eu.posthog.com',
+			person_profiles: 'always',
+			persistence: 'localStorage+cookie',
 			capture_pageview: false,
 			capture_pageleave: false,
-			capture_exceptions: true // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+			capture_exceptions: true
 		});
 	}
 	return;
 };
+load().finally();
